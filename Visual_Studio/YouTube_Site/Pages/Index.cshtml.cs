b5d2d13317168ad889f880b4cafdc1f4b02c01df
BadlandBandit <rabbitcrs@gmail.com>
2026-04-06 16:02:00 -0600

    added visual studio folder, clean up other folder into one

---
+++ Visual_Studio/YouTube_Site/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YouTube_Site.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Visual_Studio/YouTube_Site/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿const button = document.getElementById("animateBtn");
+const musicNote = document.getElementById("musicNote");
+
+if (button && musicNote) {
+    let position = 0;
+    let direction = 1;
+    let animationRunning = false;
+    let animationId;
+
+    function animateNote() {
+        position += 3 * direction;
+        musicNote.style.left = position + "px";
+
+        if (position > window.innerWidth - 120 || position < 0) {
+            direction *= -1;
+        }
+
+        animationId = requestAnimationFrame(animateNote);
+    }
+
+    button.addEventListener("click", function () {
+        if (!animationRunning) {
+            animateNote();
+            button.textContent = "Stop Animation";
+            animationRunning = true;
+        } else {
+            cancelAnimationFrame(animationId);
+            button.textContent = "Start Animation";
+            animationRunning = false;
+        }
+    });
+}
