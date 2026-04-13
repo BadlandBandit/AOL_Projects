54393b0084dd5cf947598edbd965767938b54493
BadlandBandit <rabbitcrs@gmail.com>
2026-04-13 12:24:40 -0600

    C# update

---
+++ Basic_C#_Programs/myConsoleProject.cs/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+    }
+    }
+
